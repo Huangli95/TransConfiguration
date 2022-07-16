215cec11171d33fdf44dc71582ee09c310b348f4
crit <crit@none.com>
2022-07-17 01:45:05 +0800

    feat: add project

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(int argc, char** argv)
+{
+    cout << "hello world" << endl;
+    return 0;
+}
